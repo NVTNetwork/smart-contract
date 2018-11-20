564fa76891a0200600745415f9e80fad37578516
Dimitris <dimitris@nvtnetwork.com>
2018-11-20 03:31:49 +0200

    erc20 token contract

--- NVTNetworkToken.sol
+++ NVTNetworkToken.sol
@@ -59,7 +59,7 @@ function transferFrom(
         return false;
     }
 }
-//If this function is called again it overwrites the current allowance with _value.
+// If this function is called again it overwrites the current allowance with _value.
 function approve(address _spender, uint256 _amount) returns (bool success) {
     allowed[msg.sender][_spender] = _amount;
     Approval(msg.sender, _spender, _amount);
